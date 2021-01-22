4e7bcf50ed86334d6537f9b2b8aaf05090fc6ca5
Catherinal <75525803+Catherinal@users.noreply.github.com>
2021-01-22 17:23:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,114 +1,131 @@
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
 
-var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
 
-var gameState = "onSling";
-var bg="sprites/bg1.png"
-var score=0
+  function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
 
-function preload() {
-    getBackgroundImg();
-}
+	stoneObj=new stone(235,420,30); 
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+	mango1=new mango(1100,100,30);
+  mango2=new mango(1170,130,30);
+	mango3=new mango(1010,140,30);
+	mango4=new mango(1000,70,30);
+	mango5=new mango(1100,70,30);
+	mango6=new mango(1000,230,30);
+	mango7=new mango(900,230,40);
+	mango8=new mango(1140,150,40);
+	mango9=new mango(1100,230,40);
+	mango10=new mango(1200,200,40);
+	mango11=new mango(1120,50,40);
+	mango12=new mango(900,160,40);
 
-
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
-}
-
-function draw(){
-    if (backgroundImg)
-    background(backgroundImg);
-
-    noStroke();
-    textSize(35)
-    fill("black")
-    text("score"+score,width-300,50)
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    pig1.score();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    pig3.score();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();   
-}
-
-function mouseDragged(){
-    if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	launcherObject=new launcher(stoneObj.body,{x:235,y:420})
+  var render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+      width: 1300,
+      height: 600,
+      wireframes: false
     }
+  });
+	
+	Engine.run(engine);
+ // Render.run(render);
 }
 
+function draw() {
 
-function mouseReleased(){
-    slingshot.fly();
-    gameState = "launched";
+  background(230);
+  //frameRate(2)
+ // Engine.update(engine)
+  textSize(25);
+  text("Press Space to get a second Chance to Play!!",50 ,50);
+  image(boy ,200,340,200,300);
+  //Engine.update(engine)
+  
+
+  treeObj.display();
+  stoneObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango6.display();
+ mango7.display();
+  mango8.display();
+  mango9.display();
+  mango10.display();
+  mango11.display();
+  mango12.display();
+  stoneObj.display();
+
+  groundObject.display();
+  launcherObject.display();
+  detectollision(stoneObj,mango1);
+  detectollision(stoneObj,mango2);
+  detectollision(stoneObj,mango3);
+  detectollision(stoneObj,mango4);
+  detectollision(stoneObj,mango5);
+  detectollision(stoneObj,mango6);
+  detectollision(stoneObj,mango7);
+  detectollision(stoneObj,mango8);
+  detectollision(stoneObj,mango9);
+  detectollision(stoneObj,mango10);
+  detectollision(stoneObj,mango11);
+  detectollision(stoneObj,mango12);
 }
 
-function keyPressed(){
-    if(keyCode === 32){
-       // slingshot.attach(bird.body);
+function mouseDragged()
+{
+	Matter.Body.setPosition(stoneObj.body, {x:mouseX, y:mouseY}) 
+}
+
+function mouseReleased()
+{
+	launcherObject.fly();
+    // distance=int(dist(stoneObj.x,stoneObj.y,mango1.x,mango1.y));
+}
+
+function keyPressed() {
+	if (keyCode === 32) {
+    Matter.Body.setPosition(stoneObj.body, {x:235, y:420}) 
+	  launcherObject.attach(stoneObj.body);
+	}
+  }
+
+  function detectollision(lstone,lmango){
+	/*var collision = Matter.SAT.collides(lstone,lmango);
+	if(collision.collided){
+		console.log("collided");
+		Matter.Body.setStatic(lmango,false);	
+	}*/
+  mangoBodyPosition=lmango.body.position
+  stoneBodyPosition=lstone.body.position
+  
+  var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
+  //console.log(distance)
+ // console.log(lmango.r+lstone.r)
+  	if(distance<=lmango.r+lstone.r)
+    {
+      //console.log(distance);
+  	  Matter.Body.setStatic(lmango.body,false);
     }
-}
 
-async function getBackgroundImg(){
-    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo")
-    var responseJSON=await response.json();
-    var datetime=responseJSON.datetime;
-    var hour=datetime.slice(11,13);
-
-    if (hour>=0600 && hour<=1900){
-        bg="sprites/bg1.png"
-    }
-    else{
-        bg="sprites/bg2.png"
-    }
-    backgroundImg=loadImage(bg)
-    console.log(backgroundImg);
-}
+  }
